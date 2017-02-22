0543a071fd9dd0359886bab0ceea9424b5d46f2b
Jules Prévost <julesael.prevost@gmail.com>
2017-02-22 17:49:32 +0100

    change

--- site/web/app/themes/contester/lib/setup.php
+++ site/web/app/themes/contester/lib/setup.php
@@ -89,6 +89,7 @@ function display_sidebar() {
     is_404(),
     is_front_page(),
     is_page_template('template-custom.php'),
+    is_single()
   ]);
 
   return apply_filters('sage/display_sidebar', $display);
