bc194bc42fe7cc10a66e26afe5e8809e0fa68256
Jules Prévost <julesael.prevost@gmail.com>
2017-02-22 19:11:58 +0100

    test

--- site/web/app/themes/contester/template-contact.php
+++ site/web/app/themes/contester/template-contact.php
@@ -3,7 +3,7 @@
  * Template Name: Contact
  */
 ?>
-
+<p>bonkour du template contact</p>
 <?php while (have_posts()) : the_post(); ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part('templates/content', 'page'); ?>
