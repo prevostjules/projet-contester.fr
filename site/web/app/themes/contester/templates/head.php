9359e35bba2ac763e8edbb0e59b3ca81280b102f
Jules Prévost <julesael.prevost@gmail.com>
2017-03-05 19:48:15 +0100

    super premier papier

--- site/web/app/themes/contester/templates/head.php
+++ site/web/app/themes/contester/templates/head.php
@@ -2,12 +2,8 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="https://fonts.googleapis.com/css?family=Bahiana" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Bangers&amp;subset=latin-ext" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:300,400,700&amp;subset=latin-ext" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Teko:400,500,700" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Oxygen:400,700" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Ramaraja" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
