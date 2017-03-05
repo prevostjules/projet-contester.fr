ae76985e9ef2bfdfd35c56db0716a331620060f9
Jules Prévost <julesael.prevost@gmail.com>
2017-03-05 23:54:10 +0100

    treer

--- site/web/app/themes/contester/templates/content-single.php
+++ site/web/app/themes/contester/templates/content-single.php
@@ -1,4 +1,12 @@
 <?php while (have_posts()) : the_post(); ?>
+  <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
   <article <?php post_class(); ?>>
     <header>
       <figure><img class="head-post-desktop" src<?php the_post_thumbnail(); ?></figure>
