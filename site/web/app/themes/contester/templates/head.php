528bc12a540629440e6fd0c4f0ceffa65b6d4751
Jules Prévost <julesael.prevost@gmail.com>
2017-03-09 19:15:43 +0100

    testtererefe

--- site/web/app/themes/contester/templates/head.php
+++ site/web/app/themes/contester/templates/head.php
@@ -5,5 +5,6 @@
   <link href="https://fonts.googleapis.com/css?family=Teko:400,500,700" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Oxygen:400,700" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Anton" rel="stylesheet">
   <?php wp_head(); ?>
 </head>
